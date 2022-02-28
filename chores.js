75432637f9b13396271155ea6a72fa02ad6be5ec
choreworld <choreworld@gmail.com>
2022-02-28 22:12:50 +1300

    Tidy up JavaScript and tweak layout

--- chores.js
+++ chores.js
@@ -1,35 +1,35 @@
-var START_WEEK = new Date(2021, 3, 11)
+var START_WEEK = new Date(2021, 3, 11);
 
 function assign_chores(offset, chores, people) {
-	var assignments = {}
+	var assignments = {};
 	for (var i = 0; i < chores.length; i += 1) {
-		var person = people[(i + offset) % people.length]
-		assignments[chores[i]] = person
+		var person = people[(i + offset) % people.length];
+		assignments[chores[i]] = person;
 	}
 	
-	return assignments
+	return assignments;
 }
 
 function date_to_offset(date) {
-	var weekday = date.getDay()
+	var weekday = date.getDay();
 	if (weekday) {
-		date.setDate(date.getDate() + 7 - weekday)
+		date.setDate(date.getDate() + 7 - weekday);
 	}
 
-	var diff_days = (date - START_WEEK) / (1000 * 60 * 60 * 24)
+	var diff_days = (date - START_WEEK) / (1000 * 60 * 60 * 24);
 	if (diff_days > 0) {
-		return Math.floor(diff_days / 7)
+		return Math.floor(diff_days / 7);
 	}
 
-	return 0
+	return 0;
 }
 
 function current_offset() {
-	return date_to_offset(new Date())
+	return date_to_offset(new Date());
 }
 
 function end_of_week(offset) {
-	var date = new Date(START_WEEK.getTime())
-	date.setDate(date.getDate() + 7 * offset)
-	return date
+	var date = new Date(START_WEEK.getTime());
+	date.setDate(date.getDate() + 7 * offset);
+	return date;
 }
