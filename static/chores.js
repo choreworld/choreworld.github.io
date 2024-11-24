2f79c656edcaff57b851ce3d52520772f309076e
choreworld <81790945+choreworld@users.noreply.github.com>
2024-11-25 11:01:16 +1300

    Tidy JavaScript a little
    Use camelCase, const, etc.

--- static/chores.js
+++ static/chores.js
@@ -1,35 +1,35 @@
-var START_WEEK = new Date(2021, 3, 11); // Sunday 11 APRIL 2021 (note month is 0-indexed)
+const startWeek = new Date(2021, 3, 11); // Sunday 11 APRIL 2021 (note month is 0-indexed)
 
-function assign_chores(offset, chores, people) {
-	var assignments = {};
-	for (var i = 0; i < chores.length; i += 1) {
-		var person = people[(i + offset) % people.length];
-		assignments[chores[i]] = person;
-	}
-	
+function assignChores(offset, chores, people) {
+	let assignments = {};
+	chores.forEach((chore, i) => {
+		const person = people[(i + offset) % people.length];
+		assignments[chore] = person;
+	});
+
 	return assignments;
 }
 
-function date_to_offset(date) {
-	var weekday = date.getDay();
+function dateToOffset(date) {
+	const weekday = date.getDay();
 	if (weekday) {
 		date.setDate(date.getDate() + 7 - weekday);
 	}
 
-	var diff_days = (date - START_WEEK) / (1000 * 60 * 60 * 24);
-	if (diff_days > 0) {
-		return Math.floor(diff_days / 7);
+	const diffDays = (date - startWeek) / (1000 * 60 * 60 * 24);
+	if (diffDays > 0) {
+		return Math.floor(diffDays / 7);
 	}
 
 	return 0;
 }
 
-function current_offset() {
-	return date_to_offset(new Date());
+function currentOffset() {
+	return dateToOffset(new Date());
 }
 
-function end_of_week(offset) {
-	var date = new Date(START_WEEK.getTime());
+function endOfWeek(offset) {
+	const date = new Date(startWeek.getTime());
 	date.setDate(date.getDate() + 7 * offset);
 	return date;
 }
